b66ff223b9b605e8a18ed675ff0e5210b7d6a737
Alex Kasongo <alex.kasongolenge@yahoo.com>
2020-12-28 21:18:54 -0800

    added urql for connecting to graphql

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,28 +1,31 @@
+import React, { InputHTMLAttributes } from "react";
+import { useField } from "formik";
 import {
   FormControl,
   FormLabel,
   Input,
   FormErrorMessage,
 } from "@chakra-ui/react";
-import { useField } from "formik";
-import React, { InputHTMLAttributes } from "react";
 
 // Allow inputFiled component to take any props that a regular html input would take
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
-  placeholder: string;
   name: string;
 };
 
 // '' => false
 // 'error message' => true
 
-export const InputField: React.FC<InputFieldProps> = (props) => {
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size: _,
+  ...props
+}) => {
   const [field, { error }] = useField(props);
   return (
     <FormControl isInvalid={!!error}>
-      <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
-      <Input {...field} id={field.name} placeholder={props.placeholder} />
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input {...field} {...props} id={field.name} />
       {error ? <FormErrorMessage>{field}</FormErrorMessage> : null}
     </FormControl>
   );
