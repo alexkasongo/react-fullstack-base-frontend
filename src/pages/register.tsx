3b0744d0d91b818a38ede9955973bbfe97694cd2
Alex Kasongo <alex.kasongolenge@yahoo.com>
2020-12-28 23:29:25 -0800

    update

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -6,7 +6,6 @@ import { InputField } from "../components/InputField";
 import { useRegisterMutation } from "../generated/graphql";
 import { toErrorMap } from "../utils/toErrorMap";
 import { useRouter } from "next/router";
-// import { withUrqlClient } from "next-urql";
 
 interface registerProps {}
 
